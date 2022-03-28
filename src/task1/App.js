b57b6cd1bde93f002be708e3ccc2e0f87a5aa2eb
Ran Farjun <mike232003@gmail.com>
2022-03-28 21:45:55 +0300

    first task

--- src/task1/App.js
+++ src/task1/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
+import { fetchImages } from '../api';
 import logo from '../logo.svg';
 import './App.css';
 import ImageCarousel from "./ImageCarousel";
 
 function App() {
+  const[array,setArray] = useState([])
+  const getUrls = async()=>{
+    const urls = await fetchImages();
+    setArray(urls);
+  }
+  
+
+  useEffect(()=>{
+    getUrls()
+  },[])
   return (
       <div>
-        <ImageCarousel />
+        <ImageCarousel 
+       imageArray = {array}
+        />
       </div>
   );
 }
--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,66 @@
-import React from "react";
-import { fetchImageUrls } from "../api/index";
+import React, { useState } from "react";
+import { fetchImages, fetchImageUrls } from "../api/index";
+import ArrowCircleLeftSharpIcon from '@mui/icons-material/ArrowCircleLeftSharp';
+import ArrowCircleRightSharpIcon from '@mui/icons-material/ArrowCircleRightSharp';
+
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+  const {imageArray} = props;
+  let imageArrayLength = imageArray.length;
+ 
+    const [currentImage, setCurrentImage] = useState(0);
+    const [loading, setLoading] = useState(true);
+   const HandleNextImage =()=>{
+    if(currentImage === imageArrayLength -1){
+        setCurrentImage(0);
+    }
+    else{
+        setCurrentImage(currentImage+1);
+    }
+    setLoading(true);
+   } 
+   const handlePreviusImage = ()=>{
+    if(currentImage === 0){
+        setCurrentImage(imageArrayLength - 1);
+        
+    }
+    else{
+        setCurrentImage(currentImage-1);
+        
+    }
+    setLoading(true);
+   }
+   console.log(currentImage)
+   const imageLoad = ()=>{
+       //setTimeout(setLoading(false),200)
+    setLoading(false);
+   }
+   console.log(loading);
+    return (
+        <section className='slider'> 
+        
+        <ArrowCircleLeftSharpIcon className='left-arrow'  onClick = {handlePreviusImage}/>
+        <ArrowCircleRightSharpIcon className='right-arrow' onClick = {HandleNextImage}/>
+        <div className="1" style={{display: loading ===true ? "block":"none"}}>
+       Loading images,
+       </div>
+        <div className="2" style={{display: loading ===false ? "block" : "none"}}>
+        {imageArray.map((image,index)=>{
+            return(
+                
+                <div
+            className={index === currentImage ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === currentImage && (
+              <img src={image} alt='travel image' className='image' onLoad={imageLoad}/>
+            )}
+          </div>
+            )
+        })}
+        </div>
+       
+        </section>
+        );
 };
 export default ImageCarousel;
